e092baf2d8f37e3c6a79f2120dbbc2d51d9ac8d9
lucafus <ec2-user@ip-172-31-23-249.us-east-2.compute.internal>
2023-05-30 23:07:21 +0000

    Project

--- Project/main.tf
+++ Project/main.tf
@@ -61,7 +61,7 @@ resource "aws_instance" "project_instance" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
+      "sudo yum update -y", 
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
       "sudo yum upgrade -y",
