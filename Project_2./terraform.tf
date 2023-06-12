0bbdeca6585e85e3abc811ae23b2cef7b8be896d
lucafus <luca.fusaro1996@gmail.com>
2023-06-12 01:01:39 +0000

    Project

---
+++ Project_2./terraform.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "s3" {
+    bucket = "project-week-21"
+    key    = "Project/State"
+    region = "us-east-2"
+  }
+
+
+  required_providers {
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
